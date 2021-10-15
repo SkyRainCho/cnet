9c85c41b277948512083cbd6fbf5f18933931084
caoyiming <joseph.yeeming.cho@gmail.com>
2021-10-15 18:13:49 +0800

    实现一个简单的echo服务器

--- net/client.go
+++ net/client.go
@@ -1,1 +1,71 @@
 package cnet
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type output struct {
+	data   []byte
+	length int
+
+	cond *sync.Cond
+}
+
+type client struct {
+	cn      net.Conn
+	lock    sync.Mutex
+	pending output
+}
+
+func (c *client) CloseConnect() {
+	c.cn.Close()
+}
+
+func (c *client) FillPendingData(data []byte, length int) {
+	c.pending.length += length
+	c.pending.data = append(c.pending.data, data[:length]...)
+	c.pending.cond.Signal()
+}
+
+func (c *client) FlushPendingData() {
+	fmt.Printf("Client::FlushPendingData::%d\n", c.pending.length)
+	_, err := c.cn.Write(c.pending.data[:c.pending.length])
+	if err != nil {
+		fmt.Println("Client::FlushPendingData::WriteError")
+	}
+	c.pending.length = 0
+	c.pending.data = c.pending.data[0:0]
+}
+
+func (c *client) Read() {
+	fmt.Println("Client::Read")
+
+	//创建一个读取缓冲区
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := c.cn.Read(buf)
+		if n == 0 && err != nil {
+			c.CloseConnect()
+			return
+		}
+		fmt.Println("ReadMsg:", string(buf[:n]))
+		c.FillPendingData(buf, n)
+	}
+}
+
+func (c *client) Write() {
+	fmt.Println("Client::Write")
+
+	//输出缓冲区之中没有数据的话，协程等待
+	for {
+		c.lock.Lock()
+		if c.pending.length == 0 {
+			c.pending.cond.Wait()
+		}
+		c.FlushPendingData()
+		c.lock.Unlock()
+	}
+}
--- net/server.go
+++ net/server.go
@@ -3,22 +3,22 @@ package cnet
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
-	running				bool
-	shutdown 			bool
-	listener 			net.Listener
-	
+	running  bool
+	shutdown bool
+	listener net.Listener
 
-	shutdownComplete	chan struct{}
+	shutdownComplete chan struct{}
 }
 
 func (s *Server) WaitForShutdown() {
-	<- s.shutdownComplete
+	<-s.shutdownComplete
 }
 
-func (s *Server)isRunning() bool {
+func (s *Server) isRunning() bool {
 	return s.running
 }
 
@@ -28,8 +28,48 @@ func (s *Server) Start() {
 	s.StartLoop(clientListenReady)
 }
 
-func (s *Server)StartLoop(clr chan struct{}) {
-	defer func()  {
+func (s *Server) startRoutine(f func()) bool {
+	started := false
+	if s.isRunning() {
+		go f()
+		started = true
+	}
+	return started
+}
+
+func (s *Server) createClient(conn net.Conn) *client {
+	c := &client{
+		cn: conn,
+	}
+	fmt.Println("Server::createClient")
+
+	c.lock.Lock()
+	c.pending.cond = sync.NewCond(&(c.lock))
+	c.lock.Unlock()
+
+	s.startRoutine(func() { c.Read() })
+
+	s.startRoutine(func() { c.Write() })
+	return c
+}
+
+func (s *Server) acceptConnect(createFunc func(conn net.Conn)) {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			fmt.Println("Accept Failed")
+			continue
+		}
+
+		s.startRoutine(func() {
+			fmt.Println("Prepare create client in routine")
+			createFunc(conn)
+		})
+	}
+}
+
+func (s *Server) StartLoop(clr chan struct{}) {
+	defer func() {
 		if clr != nil {
 			close(clr)
 		}
@@ -43,12 +83,8 @@ func (s *Server)StartLoop(clr chan struct{}) {
 	s.listener = l
 	s.running = true
 
-	for s.isRunning() {
-		conn, err := s.listener.Accept()
-		if err != nil {
-
-		}
-	}
+	//启动一个监听协程用于接收网络连接，创建客户端
+	go s.acceptConnect(func(conn net.Conn) { s.createClient(conn) })
 }
 
 func NewServer() (*Server, error) {
@@ -60,7 +96,7 @@ func NewServer() (*Server, error) {
 }
 
 func Run(server *Server) error {
-	
+
 	server.Start()
 
 	return nil
